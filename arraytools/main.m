95b9771242dedffa316ec5a5cd5befbdc1f92552
Manuel <manuel@vikingosegundo.de>
2011-08-28 03:19:26 +0200

    Initial Commit

---
+++ arraytools/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  arraytools
+//
+//  Created by Manuel Meyer on 28.08.11.
+//  Copyright (c) 2011 apparatschik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
